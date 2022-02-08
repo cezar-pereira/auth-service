af11f18465d2baef6d7746c80478e1d276fc8eec
cezar-pereira <helaniocezar@gmail.com>
2022-02-07 22:59:20 -0300

    added setPreferredOrientations and debugShowCheckedModeBanner to false

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() =>
-    runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+}
